42db12e67f40e5be0f2228118e9264b2de7e6cf1
julianbrowne <julian@webskill.com>
2016-06-12 09:39:13 +0100

    cleaned up basic example code

--- examples/utils.js
+++ examples/utils.js
@@ -1,53 +1,74 @@
 
 var utils = (function() { 
 
-    return { 
+    function prettyPrint(obj) { 
+        return JSON.stringify(obj, null, 2);
+    }
 
-        testObject: { 
-            name: { 
-                firstName: "John",
-                lastName: "Smith"
-            },
-            personal: { 
-                age: 42,
-                likesIceCream: true
-            },
-            hobbies: [ 
-                "football",
-                "golf"
-            ],
-            address: { 
-                road: { number: "1", street: "the street" },
-                town: "townington",
-                county: "Shireshire"
-            }
-        },
+    function parseEventName(e) { 
+        return e.type + ":" + e.namespace;
+    }
 
-        prettyPrint: function(obj) { 
-            return JSON.stringify(obj, null, 2);
-        },
+    function prepContainerForCode(elementName) { 
+        var wrapper = $("#" + elementName);
+        wrapper.append($("<pre>"));
+        wrapper.find("pre").append($("<code>"));
+        return wrapper;
+    }
 
-        prettyPrintSource: function(obj, target) { 
-
-            var target = (target === undefined) ? "json-source" : target;
-            var selector = "#" + target;
-
-            if($(selector).length===0) { 
-                $("<div>")
-                    .attr("id",target)
-                    .appendTo($("body"));
-            }
-
-            $(selector).empty();
-
-            $("<div>")
-                .attr("id", "json-source-contents")
-                .appendTo(selector);
-
-            $("#json-source-contents")
-                .html("<pre><code>" + JSON.stringify(obj, null, 2) + "</code></pre>");
+    function findAndPrepTargetElement(elementName) { 
+        var wrapper = $("#" + elementName);
+        if(wrapper.find("pre").length === 0) { 
+            prepContainerForCode(elementName);
         }
+        return wrapper.find("code");
+    }
 
+    function logEvent(container) { 
+        var target = findAndPrepTargetElement(container);
+        return function(e) { 
+            target.append(parseEventName(e) + "\n");
+        }
+    }
+
+    function logObjectChangeEvent(container) { 
+        var target = findAndPrepTargetElement(container);
+        return function(e, obj) { 
+            target.html(prettyPrint(obj));
+        }
+    }
+
+    function prettyPrintSource(obj, container) { 
+        var target = findAndPrepTargetElement(container);
+        target.html(prettyPrint(obj));
+    }
+
+    var testObject = { 
+        name: { 
+            firstName: "John",
+            lastName: "Smith"
+        },
+        personal: { 
+            age: 42,
+            likesIceCream: true
+        },
+        hobbies: [ 
+            "football",
+            "golf"
+        ],
+        address: { 
+            road: { number: "1", street: "the street" },
+            town: "townington",
+            county: "Shireshire"
+        }
+    };
+
+    return { 
+        logEvent: logEvent,
+        logObjectChangeEvent: logObjectChangeEvent,
+        prettyPrint: prettyPrint,
+        testObject: testObject,
+        prettyPrintSource: prettyPrintSource
     };
 
 }());
