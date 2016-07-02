75e3e4757faf0351a1d739630a2ddd0e6f79882d
julianbrowne <julian@webskill.com>
2016-07-02 07:28:34 +0100

    test refactor

--- test/spec/medea.core.js
+++ test/spec/medea.core.js
@@ -300,44 +300,6 @@ describe("Medea", function() {
 
     });
 
-    describe("Events", function() { 
-
-        /**
-         *  @todo: two tests here: one for medea.add with buttons
-         *  and one without
-        **/
-
-        it("should respond to a 'medea.add' event", function() { 
-            medeaHelper.addTestContainer("test");
-            var container = $("#test");
-            container.medea({ abc: 123});
-            var match = medeaHelper.matcher();
-            expect($("form").length).toEqual(1);
-            expect(match.allInputs.length).toEqual(1);
-            // add 1 field
-            container.trigger("medea.add");
-            var match = medeaHelper.matcher();
-            expect($("form").length).toEqual(1);
-            expect(match.allInputs.length).toEqual(2);
-            medeaHelper.removeTestContainer("test");
-        });
-
-        it("should fire 'medea.submit' event on form submit", function() { 
-            medeaHelper.addTestContainer("test");
-            var container = $("#test");
-            container.medea({ fieldOne: 55});
-            var form = $("form");
-            var spy = spyOnEvent(container, "medea.submit");
-            var submitHandler = jasmine.createSpy().and.returnValue(false);
-            form.submit(submitHandler);
-            form.trigger("submit");
-            expect("medea.submit").toHaveBeenTriggeredOn(container);
-            expect(spy).toHaveBeenTriggered();
-            medeaHelper.removeTestContainer("test");
-        });
-
-    });
-
     describe("Interactions", function() { 
 
         it("should delete a form field", function() { 
